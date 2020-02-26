fec81e82b955a6683fe263806b5cce73d974ede9
Wasek <wesinem@gmail.com>
2020-02-25 23:22:48 -0800

    integrated rsuite library

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+
+import 'rsuite/dist/styles/rsuite-default.css';
+import { Button } from 'rsuite';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,7 +21,8 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <Button appearance="primary"> Hello world </Button>
     </div>
   );
 }
