cc682cff05dbe652476914a0a25cd4ba7b674a4f
Wasek <wesinem@gmail.com>
2020-03-01 01:51:52 -0800

    added sidenav and landing page

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
 import React from 'react';
-// import logo from './logo.svg';
-import './App.css';
+import LandingPage from './pages/LandingPage';
 
-import 'rsuite/dist/styles/rsuite-default.css';
-import { Container, Header, Content, Footer, Sidebar } from 'rsuite';
 
 function App() {
   return (
-    <div className="App">
-      <Container>
-        <Sidebar style = {{background: 'blue'}}>Sidebar</Sidebar>
-        <Container>
-          <Header style = {{background: 'red'}}>Header</Header>
-          <Content style = {{background: 'yellow'}}>Content</Content>
-          <Footer style = {{background: 'grey'}}>Footer</Footer>
-        </Container>
-      </Container>
+    <div>
+      <LandingPage />
     </div>
   );
 }
