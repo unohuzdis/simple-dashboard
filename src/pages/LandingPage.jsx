c8a35af4ae5172f5c47ec9633bedfcdc1c6e643f
Wasek <wesinem@gmail.com>
2020-03-03 13:58:33 -0800

    added responsiveness

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -7,12 +7,13 @@ import TopNav from '../components/top-nav/TopNav';
 import MainContent from '../components/content/MainContent';
 import CustomFooter from '../components/footer/CustomFooter';
 
-class LandingPage extends React.Component {
+class LandingPage extends React.PureComponent {
     constructor() {
         super();
 
         this.state = {
-            clickedContent: 'Dashboard'
+            clickedContent: 'Dashboard',
+            expanded: true
         };
     }
 
@@ -20,23 +21,38 @@ class LandingPage extends React.Component {
         this.setState({ clickedContent: event });
     };
 
+    // Throttling can be used for performance optimization
+    handleWindowResize = () => {
+        this.setState({ expanded: window.innerWidth > 600 });
+    }
+
+    componentDidMount() {
+        window.addEventListener('resize', this.handleWindowResize);
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('resize', this.handleWindowResize);
+    }
+
     render() {
         return (
             <div>
                 <Container style={{ background: "#F7F8FA", height: "100vh" }}>
-                    <Sidebar>
-                        <SideNav onSelect={this.handleClick} />
+                    <Sidebar style={{ display: 'flex', flexDirection: 'column' }}
+                        width={this.state.expanded ? 260 : 56}
+                        collapsible>
+                        <SideNav onSelect={this.handleClick} expand={this.state.expanded} />
                     </Sidebar>
                     <Container>
                         <Header>
                             <TopNav appearance="inverse"
-                                    onSelect = {this.handleClick}
+                                onSelect={this.handleClick}
                             />
                         </Header>
                         <Content style={{ background: 'white' }}>
                             <MainContent title={this.state.clickedContent} />
                         </Content>
-                        <Footer> 
+                        <Footer>
                             <CustomFooter />
                         </Footer>
                     </Container>
